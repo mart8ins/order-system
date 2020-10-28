fdc287fcdc142a67da756db622ea05a02981e3fa
Martins Skinkis <mart8ins@gmail.com>
2020-10-28 20:33:58 +0200

    improved new models components form  with cleaner code

--- src/app/model-control/add-new-model/add-new-model.component.ts
+++ src/app/model-control/add-new-model/add-new-model.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { InputValidators } from '../../validators/input.validators';
+// import { Validators } from '@angular/forms';
+// import { InputValidators } from '../../validators/input.validators';
 
 // helper data to render all needed values in select options
 import { Brands } from "../../utilities/brands";
@@ -28,69 +28,26 @@ export class AddNewModelComponent implements OnInit {
     this.applianceTypes = AppliancesType;
   }
 
+  submit(form) {
+    console.log(form)
+    if (form.controls.brand.value && form.controls.model.value && form.controls.type.value) {
+      {
+        let userid = this.modelService.modelCreatedByUserId();
 
-  form = new FormGroup(
-    {
-      brand: new FormControl('',
-        [
-          Validators.required,
-          InputValidators.cannotContainSpace,
-          InputValidators.cannotContainNumber
-        ]
-      ),
-      model: new FormControl('',
-        [
-          Validators.required,
-          InputValidators.cannotContainSpace
-        ]
-      ),
-      applianceType: new FormControl('', Validators.required),
-      description: new FormControl('')
-    }
-  )
-
-  get brand() {
-    return this.form.get('brand')
-  }
-  get model() {
-    return this.form.get('model')
-  }
-  get applianceType() {
-    return this.form.get('applianceType')
-  }
-  get description() {
-    return this.form.get('description')
-  }
-
-
-
-  formSubmit = (form) => {
-    console.log(form.value)
-    if (form.value.brand && form.value.model && form.value.applianceType) {
-      let userid = this.modelService.modelCreatedByUserId();
-
-      let addModel = {
-        createdByUserId: userid,
-        brand: form.value.brand,
-        model: form.value.model.toUpperCase(),
-        applianceType: form.value.applianceType,
-        description: form.value.description
+        let addModel = {
+          createdByUserId: userid,
+          brand: form.controls.brand.value,
+          model: form.controls.model.value.toUpperCase(),
+          applianceType: form.controls.type.value,
+          description: form.controls.descriptionvalue
+        }
+        this.modelService.storeModelToLS(addModel);
+        // for info msg about added model to data base
+        this.latestAddedModel = `${addModel.brand} ${addModel.model}`
       }
-
-      this.modelService.storeModelToLS(addModel);
-
-      // for info msg about added model to data base
-      this.latestAddedModel = `${addModel.brand} ${addModel.model}`
-
-      // clears the form after submit, and also removes validation errors
-      this.form.controls.brand.setValue('');
-      this.form.controls.brand.markAsUntouched();
-      this.form.controls.model.setValue('');
-      this.form.controls.model.markAsUntouched();
-      this.form.controls.applianceType.setValue('');
-      this.form.controls.applianceType.markAsUntouched();
-      this.form.controls.description.setValue('');
-      this.form.controls.description.markAsUntouched();
     }
+    form.reset();
   }
+
+
 }
